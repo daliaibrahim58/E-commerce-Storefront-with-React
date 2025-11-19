796be13abbecf6d5c142a5d0f7ff2c05809d980a
daliaibrahim58 <daliaibrahim582005@gmail.com>
2025-11-19 07:50:30 +0200

    Updated routes and fixed navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Signup from "./components/SignUp";
 import SignIn from "./components/SignIn";
-import ContactUs from "./components/ContactUs"; // Component is imported
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<SignIn />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
     </>
   );
 }

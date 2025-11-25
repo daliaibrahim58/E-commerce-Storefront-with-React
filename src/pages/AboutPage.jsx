865fd26fc5197cb3feb21941be00bde5e16e391c
mohamed <hajralhaj346@gmail.com>
2025-11-25 03:37:32 +0200

    deploy to vercel

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,4 @@
-import AboutHero from "../components/about/abouthero";
+import AboutHero from "../components/about/AboutHero";
 import CTA from "../components/about/cta";
 import Mission from "../components/about/Mission";
 import Values from "../components/about/Values";
